9de6435732603c55f7a32318dab53a3fbae6708d
mdnaufalh <33623367+mdnaufalh@users.noreply.github.com>
2020-06-03 19:08:15 +0530

    Update RMQ.h

--- Data Structures/RMQ.h
+++ Data Structures/RMQ.h
@@ -6,13 +6,15 @@
 template<class T>
 struct RMQ {
     vector<vector<T>> jmp;
-    RMQ(const vector<T>& V) : jmp(1, V) {
-        for (int pw = 1, k = 1; pw * 2 <= sz(V); pw *= 2, ++k) {
-            jmp.emplace_back(sz(V) - pw * 2 + 1);
-            for(int j = 0; j < (int)jmp[k].size(); ++j)
+
+    RMQ(const vector<T> &V) : jmp(1, V) {
+        for (int pw = 1, k = 1; pw * 2 <= (int) V.size(); pw *= 2, ++k) {
+            jmp.emplace_back((int) V.size() - pw * 2 + 1);
+            for (int j = 0; j < (int) jmp[k].size(); ++j)
                 jmp[k][j] = min(jmp[k - 1][j], jmp[k - 1][j + pw]);
         }
     }
+
     T query(int a, int b) {
         assert(a < ++b);
         int dep = 31 - __builtin_clz(b - a);
